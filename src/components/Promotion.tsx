73c304008e95a904f28469769ff0d2a2d686f6aa
heispapijay <heispapijay@gmail.com>
2024-02-05 17:24:53 +0200

    Update Promotion component styles

--- src/components/Promotion.tsx
+++ src/components/Promotion.tsx
@@ -3,7 +3,7 @@ import './styles/trycss.css';
 
 export const Promotion = () => {
   return (
-    <div className='flex justify-center items-center gap-2 p-4 bg-orange50 rounded-lg'>
+    <div className='flex justify-center items-center gap-1 sm:gap-2 p-4 bg-orange50 rounded-lg'>
         <p className='text-white text-14 lg:text-lg'>Free Courses 🌟 Sale Ends Soon, Get It Now</p>
         <FaArrowRight style={{color: 'white'}} />
     </div>
